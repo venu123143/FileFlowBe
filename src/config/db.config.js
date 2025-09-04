87820df5cb0551e6c839ac8254840cc45ab4c4e8
venu123143 <venugopalreddy9493@gmail.com>
2025-09-04 16:19:52 +0530

    for safe side migration added the js file as well

---
+++ src/config/db.config.js
@@ -0,0 +1,34 @@
+// src/config/dbConfig.js
+require('dotenv').config();
+
+const dbConfig = {
+  development: {
+    username: process.env.DEV_DB_USERNAME,
+    password: process.env.DEV_DB_PASSWORD,
+    database: process.env.DEV_DB_DATABASE,
+    host: process.env.DEV_DB_HOST,
+    dialect: 'postgres',
+    port: parseInt(process.env.DEV_DB_PORT || '5432', 10),
+    timezone: '+00:00',
+  },
+  test: {
+    username: process.env.TEST_DB_USERNAME,
+    password: process.env.TEST_DB_PASSWORD,
+    database: process.env.TEST_DB_DATABASE,
+    host: process.env.TEST_DB_HOST,
+    dialect: 'postgres',
+    port: parseInt(process.env.TEST_DB_PORT || '5432', 10),
+    timezone: '+00:00',
+  },
+  production: {
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_DATABASE,
+    host: process.env.PROD_DB_HOST,
+    dialect: 'postgres',
+    port: parseInt(process.env.PROD_DB_PORT || '5432', 10),
+    timezone: '+00:00',
+  },
+};
+
+module.exports = dbConfig;
