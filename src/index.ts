9bdf81943131a9c7f397190adcdb929c5b17f37a
venu123143 <venugopalreddy9493@gmail.com>
2026-03-07 14:50:28 +0530

    code changes

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,12 @@ import config from "@/config/config";
 const app = new App();
 const socketEngine = app.getSocketEngine();
 const port = config.PORT || 3000;
-const hostname = process.env.IP || "0.0.0.0"; // Use IP from env or default to 0.0.0.0 (all interfaces)
+const hostname = "0.0.0.0"; // Use IP from env or default to 0.0.0.0 (all interfaces)
 
 // Start Bun server
 Bun.serve({
   port,
-  hostname,
+  // hostname,
   idleTimeout: 30,
   websocket: socketEngine.handler().websocket, // <-- attach WebSocket handler
   async fetch(req, server) {
